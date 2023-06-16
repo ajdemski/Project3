cc5e80ccf4bd1746b8274441121388d80c56c7e8
Aaron Demski <ajdemski@frontier.com>
2023-06-15 17:20:20 -0700

    initial set-up

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("numberInput");
+  const rogobersAnswer = document.getElementById("numberOutput");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const num = parseInt(document.getElementById("input").value);
+    const result = [];
+
+  })
+})
