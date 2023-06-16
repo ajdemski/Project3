a2ebeb22f217519bc48ca65c51b51a26811f731a
Aaron Demski <ajdemski@frontier.com>
2023-06-15 17:47:59 -0700

    ran into error's having to do with dumb typo's

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("numberInput");
-  const rogobersAnswer = document.getElementById("numberOutput");
+  const form = document.getElementById("wordCounter");
+  const rogobersAnswer = document.getElementById("rogobersAnswer");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     const num = parseInt(document.getElementById("input").value);
     const result = [];
 
-  })
-})
+    for (let i = 0; i <= num; i++) {
+      let message = "";
+      if (i.toString().includes("1")) {
+        message += "Won't you be my neighbor?";
+      } else {
+        if (i.toString().includes("2")) {
+          message += "boop";
+        }
+        if (i.toString().includes("3")) {
+          message += "beep";
+        }
+      }
+      if (message === "") {
+        message = i.toString();
+      }
+      result.push(message);
+    }
+
+    rogobersAnswer.textContent = result.join(" ");
+  });
+});
